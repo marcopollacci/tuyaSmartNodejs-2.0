e54a88a3782c05ae0d4b3bb383e6fd826d2970be
Marco Pollacci <marco.pollacci@lepida.it>
2020-11-18 09:48:21 +0100

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  const options = new DocumentBuilder()
+  .setTitle('Tuya Call')
+  .setDescription('TuyaCall API')
+  .setVersion('1.0')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
